72e243d90e1eed5ed4c6385b695bd894441cd985
Tonychris115 <113699865+Tonychris115@users.noreply.github.com>
2022-11-18 16:39:18 +0530

    Add files via upload

--- Project Development Phase/Sprint 2/Source Code/home.php
+++ Project Development Phase/Sprint 2/Source Code/home.php
@@ -1,84 +1,132 @@
-<?php require_once "controllerUserData.php"; ?>
-<?php 
-$email = $_SESSION['email'];
-$password = $_SESSION['password'];
-if($email != false && $password != false){
-    $sql = "SELECT * FROM usertable WHERE email = '$email'";
-    $run_Sql = mysqli_query($conn, $sql);
-    if($run_Sql){
-        $fetch_info = mysqli_fetch_assoc($run_Sql);
-        $status = $fetch_info['status'];
-        $code = $fetch_info['code'];
-        if($status == "verified"){
-            if($code != 0){
-                header('Location: reset-code.php');
-            }
-        }else{
-            header('Location: user-otp.php');
-        }
-    }
-}else{
-    header('Location: login-user.php');
-}
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
 <style>
-body {
-background-image:url('pics/homes.jpg');
- background-repeat:no-repeat;
- background-size:cover;
- }
- </style>
-  <head>
-    <meta charset="utf-8">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  </head>
-  <body>
-    <div class="wrapper">
-      <input type="checkbox" id="btn" hidden>
-      <label for="btn" class="menu-btn">
-        <i class="fas fa-bars"></i>
-        <i class="fas fa-times"></i>
-      </label>
-      <nav id="sidebar">
-        <div class="title">Menu</div>
-        <ul class="list-items">
-          <li><a href="#"><i class="fas fa-user"></i>Profile</a></li>
-          <li><a href="#"><i class="fas fa-sliders-h"></i>Accounts</a></li>
-          <li><a href="#"><i class="fas fa-address-book"></i>Expense</a></li>
-          <li><a href="#"><i class="fas fa-stream"></i>Budget Limit</a></li>
-          <li><a href="#"><i class="fas fa-cog"></i>Settings</a></li>
-          <li><a href="#"><i class="fas fa-globe-asia"></i>Feedback</a></li>
-          <li><a href="#"><i class="fas fa-envelope"></i>Support</a></li>
-          <li><a href="logout-user.php"><i class="fas fa-home"></i>Logout</a></li>
-          <div class="icons">
-            <a href="#"><i class='fab fa-google-play'></i></a>
-            <a href="#"><i class="fab fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fa-github"></i></a>
-            <a href="#"><i class="fab fa-youtube"></i></a>
+  .info-tooltip,.info-tooltip:focus,.info-tooltip:hover{
+    background:unset;
+    border:unset;
+    padding:unset;
+  }
+</style>
+<h1>Welcome to <?php echo $_settings->info('name') ?></h1>
+<hr>
+<div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-money-bill-alt"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Current Overall Budget</span>
+                <span class="info-box-number text-right">
+                  <?php 
+                    $cur_bul = $conn->query("SELECT sum(balance) as total FROM `categories` where status = 1 ")->fetch_assoc()['total'];
+                    echo number_format($cur_bul);
+                  ?>
+                  <?php ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
           </div>
-        </ul>
-      </nav>
-    </div>
-    <div>
-    <nav>
-      <link rel="stylesheet" href="new.css">
-      <input type="checkbox" id="check">
-      <label for="check" class="checkbtn">
-        <i class="fas fa-bars"></i>
-      </label>
-      <label class="logo">Expense Tracker</label>
-      <ul>
-        <li><a class="active" href="#">Add Income</a></li>
-        <li><a href="#">Add Expense</a></li>
-        <li><a href="#">Edit Income</a></li>
-        <li><a href="#">Edit Expense</a></li>
-        <li><a href="#">Sync Bank Account</a></li>
-      </ul>
-    </nav>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-calendar-day"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Today's Budget Entries</span>
+                <span class="info-box-number text-right">
+                  <?php 
+                    $today_budget = $conn->query("SELECT sum(amount) as total FROM `running_balance` where category_id in (SELECT id FROM categories where status =1) and date(date_created) = '".(date("Y-m-d"))."' and balance_type = 1 ")->fetch_assoc()['total'];
+                    echo number_format($today_budget);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+
+          <!-- fix for small devices only -->
+          <div class="clearfix hidden-md-up"></div>
+
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-calendar-day"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Today's Budget Expenses</span>
+                <span class="info-box-number text-right">
+                <?php 
+                    $today_expense = $conn->query("SELECT sum(amount) as total FROM `running_balance` where category_id in (SELECT id FROM categories where status =1) and date(date_created) = '".(date("Y-m-d"))."' and balance_type = 2 ")->fetch_assoc()['total'];
+                    echo number_format($today_expense);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+        </div>
+<div class="row">
+  <div class="col-lg-12">
+    <h4>Current Budget in each Categories</h4>
+    <hr>
+  </div>
 </div>
-  </body>
-</html>
+<div class="col-md-12 d-flex justify-content-center">
+  <div class="input-group mb-3 col-md-5">
+    <input type="text" class="form-control" id="search" placeholder="Search Category">
+    <div class="input-group-append">
+      <span class="input-group-text"><i class="fa fa-search"></i></span>
+    </div>
+  </div>
+</div>
+<div class="row row-cols-4 row-cols-sm-1 row-cols-md-4 row-cols-lg-4">
+  <?php 
+  $categories = $conn->query("SELECT * FROM `categories` where status = 1 order by `category` asc ");
+    while($row = $categories->fetch_assoc()):
+  ?>
+  <div class="col p-2 cat-items">
+    <div class="callout callout-info">
+      <span class="float-right ml-1">
+        <button type="button" class="btn btn-secondary info-tooltip" data-toggle="tooltip" data-html="true" title='<?php echo (html_entity_decode($row['description'])) ?>'>
+          <span class="fa fa-info-circle text-info"></span>
+        </button>
+      </span>
+      <h5 class="mr-4"><b><?php echo $row['category'] ?></b></h5>
+      <div class="d-flex justify-content-end">
+        <b><?php echo number_format($row['balance']) ?></b>
+      </div>
+    </div>
+  </div>
+  <?php endwhile; ?>
+</div>
+<div class="col-md-12">
+  <h3 class="text-center" id="noData" style="display:none">No Data to display.</h3>
+</div>
+<script>
+  function check_cats(){
+    if($('.cat-items:visible').length > 0){
+      $('#noData').hide('slow')
+    }else{
+      $('#noData').show('slow')
+    }
+  }
+  $(function(){
+    $('[data-toggle="tooltip"]').tooltip({
+      html:true
+    })
+    check_cats()
+    $('#search').on('input',function(){
+      var _f = $(this).val().toLowerCase()
+      $('.cat-items').each(function(){
+        var _c = $(this).text().toLowerCase()
+        if(_c.includes(_f) == true)
+          $(this).toggle(true);
+        else
+          $(this).toggle(false);
+      })
+    check_cats()
+    })
+  })
+</script>
